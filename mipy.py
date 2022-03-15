21537122a5d2dce68caa25d3a1723056e0d87f4b
Cm0nsters <cmonstersschroepfer@gmail.com>
2022-03-15 01:25:36 -0500

    non-bruteforce write and card dumping

--- mipy.py
+++ mipy.py
@@ -1,7 +1,9 @@
 import serial
 from serial.tools import list_ports
 import time
+import re
 
+#TODO - fix pyserial doing ASCII over pure hex data in returns (possible translator function??)
 class Reader:
     def __init__(self,port):
         self.port = port
@@ -29,7 +31,6 @@ class Reader:
     beep_enabled = 1
     delay = 0.03125
     card_key = None
-    blank_block = b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
 
     serial_close = lambda self: self.ser.close()
     manufacture = lambda self: self.read_card(0,0)
@@ -37,7 +38,7 @@ class Reader:
     def serial_open(self):
         self.ser.port = str(self.port)
         self.ser.baudrate = 9600
-        self.ser.timeout = 1
+        self.ser.timeout = 0.25
         self.ser.open()
 
     def beep(self):
@@ -82,17 +83,15 @@ class Reader:
         self.beep()
         return resp[-17:-1]
 
-    def write_card(self,sector,block,data=blank_block):
-        #TODO - finish comparing checksum implementation with testwrite.py
+    def write_card(self,sector,block,data=b""):
         writebyte = bytearray([0x13,0x07,int(hex((4*sector)+block),16)])
    
-        #card data
+        #card data handler
         if len(data) > 16:
             return "Data longer than 16 char/hex!"
 
         while len(data) < 16:
             data += b"\x00"
-            print(data)
         
         #checksum calc
         checksum_bit = 0x1A
@@ -100,12 +99,15 @@ class Reader:
         checksum_bit += sector * 4
         for b in data:
             checksum_bit += b
-            print(checksum_bit)
 
         while checksum_bit > 0xFF:
             checksum_bit -= 0x100
         
-        print(hex(checksum_bit))
+        #append data
+        for bit in data:
+            writebyte.append(bit)
+
+        writebyte.append(checksum_bit)
 
         self.card_check()
         self.ser.write(self.nfc_protocol["starter"])
@@ -113,12 +115,25 @@ class Reader:
         time.sleep(self.delay)
         self.ser.write(bytearray([0x0A,0x05,int(hex(sector),16),0x03,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,int(hex(sector+12),16)])) #final key
         time.sleep(self.delay)
-        writebyte.append(checksum_bit)
         self.ser.write(writebyte)
         time.sleep(self.delay)
-
         self.beep()
         return "Write Complete!"
+
+    def dump(self):
+        filename = f"{self.anticollision()} - {time.asctime()}.txt".replace(":","-")
+        filename_final = re.sub("\\\\x|b'|'","",filename)
+        dump_file = open(filename_final,"w")
+        time.sleep(self.delay)
+        for i in range(16):
+            dump_file.write(f"Sector {i}\n")
+            for j in range(4):
+                data = self.read_card(i,j)
+                dump_file.write(f"{data}\n")
+                print(f"Progress: {j+(i*4)+1}/64")
+
+        dump_file.close()
+        return "Dump Complete!"
 
 
 
@@ -146,10 +161,11 @@ def main():
     "anticollision": nfc_reader.anticollision,
     "manufacture": nfc_reader.manufacture,
     "read": nfc_reader.read_card,
-    "write": nfc_reader.write_card
+    "write": nfc_reader.write_card,
+    "format": nfc_reader.write_card
     }
 
-    #TODO - clean up command portion
+    #TODO - clean up command portion heavily
     print("Enter \"help\" for a list of commands")
     while True:
         nfc_reader.ser.reset_input_buffer()
@@ -175,13 +191,18 @@ def main():
                 print("Invalid Command!")
             except IndexError:
                 print("Incomplete command!")
+        elif command_tokens[0] == "format":
+            try:
+                print(commands[command_tokens[0]](int(command_tokens[1]),int(command_tokens[2])))
+            except KeyError:
+                print("Invalid Command!")
+            except IndexError:
+                print("Incomplete command!")
         else:
             try:
                 print(commands[command_tokens[0]]())
             except KeyError:
                 print("Invalid Command!")
-
-
 
 if __name__ == "__main__":
     main()
